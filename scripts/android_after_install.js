2948c0c32afd36705caeeeb8e82aacf25b538132
Zhengz <zhengz@masn.cn>
2015-08-01 21:57:09 +0800

    fix bug

--- scripts/android_after_install.js
+++ scripts/android_after_install.js
@@ -15,7 +15,7 @@ module.exports = function(context){
     if(content.indexOf('import com.mobishift.plugins.urlscheme.URLScheme') === -1){
         content = content.replace('import org.apache.cordova.*', 'import org.apache.cordova.*;\nimport com.mobishift.plugins.urlscheme.URLScheme');
 
-        content = content.replace('loadUrl(launchUrl);', 'if(getIntent().getData() != null) { URLScheme.setUrl( getIntent().getData().toString());}\nloadUrl(launchUrl));');
+        content = content.replace('loadUrl(launchUrl);', 'if(getIntent().getData() != null) { URLScheme.setUrl( getIntent().getData().toString());}\nloadUrl(launchUrl);');
         if(content.indexOf('onNewIntent') === -1){
             content = content.replace('@Override', '@Override\npublic void onNewIntent(android.content.Intent intent){ super.onNewIntent(intent);\nif(intent.getData() != null){URLScheme.setUrl(intent.getData().toString());}\n}\n@Override');
         }else{
